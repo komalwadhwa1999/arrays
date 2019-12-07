a4a39f0dfc8311b9164e1cea0ff96dc4aaaa9304
Komal Wadhwa <57387910+komalwadhwa1999@users.noreply.github.com>
2019-12-08 03:39:07 +0530

    c file created

---
+++ Kth_largest_element.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int kthLargest(int arr[], int size, int k)
+{
+//sort in descending order
+  for(int i=0;i<size;i++)
+  {
+    for(int j=i+1;j<size;j++)
+    {
+      if(arr[i]<arr[j])
+      {
+        int t=arr[i];
+        arr[i]=arr[j];
+        arr[j]=t;
+      }
+    }
+  }
+  return arr[k];
+}
+int main()
+{
+  int k,n;
+  scanf("%d",&k);
+  scanf("%d",&n);
+  int a[n];
+  for(int i=0;i<n;i++)
+  scanf("%d",&a[i]);
+  int element=kthLargest(a ,size, k);
+  printf("Kth Largest Element = %d",element);
+  return 0;
+}
